e63a444de657e347ed0ab9b8a339c7a33c1c7f0f
Rajib Hossain <rajibhossain8521@gmail.com>
2018-11-11 11:43:17 +0600

    Add Problem Solving folder

---
+++ Problem_solving/12_factoriaL100.py
@@ -0,0 +1,7 @@
+"""
+    need to doing the mathematical calculation
+    to find the solution that how to find the
+    total last zero of the huge int number
+
+    then, finally write down the code here
+"""
